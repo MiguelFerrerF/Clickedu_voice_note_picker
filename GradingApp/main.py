4574ff166482d12d08249be471c1fa68c7a34e36
Miguel Ferrer <mferrer@inbiot.es>
2026-04-09 18:31:34 +0200

    Disables debug mode in app run configuration
    Updates the application run configuration to disable debug mode. This change is likely intended for a production environment to prevent unintended debug output or behavior.

--- GradingApp/main.py
+++ GradingApp/main.py
@@ -9,4 +9,4 @@ from gui.app_window import WebViewApp
 if __name__ == "__main__":
     app = WebViewApp()
     # Inicia la aplicación con la pantalla de login por defecto.
-    app.run(debug=True, start_url='login.html')
+    app.run(debug=False, start_url='login.html')
